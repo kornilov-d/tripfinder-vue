7f95172ad1ff61ddbf68458f97bcfefb5862289c
kornilov-d <kornilov.labs@gmail.com>
2020-06-17 04:07:58 +0300

    fixed routing

--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -1,33 +1,15 @@
 <template>
   <div class="mt-3">
     <b-card-group columns >
-      <b-card
-        title="Клёвый маршрут"
-        img-src="https://api.mapbox.com/styles/v1/kornilovd/ckbh6y4k008bo1iqsx696iwxw/static/30.2833,59.9391,13.13,0/300x150@2x?access_token=pk.eyJ1Ijoia29ybmlsb3ZkIiwiYSI6ImNrOTJuZG9hcjAxbTczbG8yNDhlZGZpOWEifQ.EMvlWY1YaawgAIHcY9n5Cw">
-        <b-card-text
-          router-link to = "/routeinfo">
-          Описание первого маршрута
-        </b-card-text>
-      </b-card>
-      <b-card title="Понтовые места"
-      img-src="https://api.mapbox.com/styles/v1/kornilovd/ckbh6y4k008bo1iqsx696iwxw/static/30.2886,59.9511,13.13,0/300x150@2x?access_token=pk.eyJ1Ijoia29ybmlsb3ZkIiwiYSI6ImNrOTJuZG9hcjAxbTczbG8yNDhlZGZpOWEifQ.EMvlWY1YaawgAIHcY9n5Cw">
-        <b-card-text>
-          Еще один маршрут
-        </b-card-text>
-      </b-card>
       <b-card v-for="tourroute in tourroutes">
         <b-card-title>
-          {{tourroute.route_name}}
+          <router-link :to="{path: 'route', query: {id: tourroute.id}}" >
+            {{tourroute.route_name}}
+          </router-link>
         </b-card-title>
-        <b-card-text>
-          {{tourroute.route_desc}}
-        </b-card-text>
+        <b-card-text>{{tourroute.route_desc}}</b-card-text>
       </b-card>
-
-      </b-card-group>
-      <br>
-
-
+    </b-card-group>
   </div>
 </template>
 
