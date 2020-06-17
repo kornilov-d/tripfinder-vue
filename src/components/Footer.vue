7e0b04663ab42599026d0b7ac38499b441859fd6
kornilov-d <kornilov.labs@gmail.com>
2020-06-17 04:13:12 +0300

    fixed layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,13 @@
 <template>
     <div class = "footer">
+      <hr>
       <b-container>
         <b-row>
-          <hr>
           <b-col cols="6" sm="6" md="4" lg="3" xl="3">
             <img src="../assets/footer.png" class="footer-logo">
+          </b-col>
+          <b-col cols="6" sm="6" md="8" lg="9" xl="9">
+
           </b-col>
         </b-row>
       </b-container>
@@ -18,13 +21,15 @@
 </script>
 
 <style scoped>
+  .footer{
+    width: 100%;
+  }
   .footer-logo{
     max-height: 2em;
   }
 
   .footer{
     margin-bottom: 1.5em;
-    position: absolute;
-    bottom: 0;
+    max-height: 4em;
   }
 </style>
