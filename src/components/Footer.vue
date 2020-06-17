12e4d2dd12013fa53a6fc0540d93ee067cd4722c
kornilov-d <kornilov.labs@gmail.com>
2020-06-17 04:21:35 +0300

    fixed layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,11 @@
 <template>
     <div class = "footer">
-      <hr>
-      <b-container>
-        <b-row>
-          <b-col cols="6" sm="6" md="4" lg="3" xl="3">
-            <img src="../assets/footer.png" class="footer-logo">
-          </b-col>
-          <b-col cols="6" sm="6" md="8" lg="9" xl="9">
 
-          </b-col>
-        </b-row>
-      </b-container>
+      <b-navbar fixed = "bottom" type="dark" variant="dark">
+        <hr>
+            <img src="../assets/footer.png" class="footer-logo">
+
+      </b-navbar>
     </div>
 </template>
 
